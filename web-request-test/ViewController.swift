0fe75b92be47ee65884f82eb2c59cad8b8674426
Ross <ross.russell.ca@gmail.com>
2016-08-29 12:36:39 -0700

    first

--- web-request-test/ViewController.swift
+++ web-request-test/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+  
+  var webView: WKWebView!
+  
+  @IBOutlet weak var loadGossip: UIButton!
+  
+  
+  @IBAction func loadSteak(sender: AnyObject) {
+    loadRequest("https://en.wikipedia.org/wiki/Steak")
+  }
+  
+  
+  @IBAction func loadGos(sender: AnyObject) {
+    loadRequest("http://radaronline.com/")
+  }
+  
+  
+  @IBAction func loadSwift(sender: AnyObject) {
+    loadRequest("https://developer.apple.com/swift/blog/")
+  }
+  
+  
+  func loadRequest( urlStr: String ){
+    
+    let url = NSURL(string: urlStr)
+    let request = NSURLRequest(URL: url!)
+    webView.loadRequest(request)
+  }
+  
+  
+  @IBOutlet weak var container: UIView!
+  
+  //called before constraints are applied?@!#
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    webView = WKWebView()
+    container.addSubview(webView)
+}
+  
+  override func viewDidAppear(animated: Bool) {
+    let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+    webView.frame = frame
+    let urlStr = "https://developer.apple.com/swift/blog/"
+    let url = NSURL(string: urlStr)
+    let request = NSURLRequest(URL: url!)
+    webView.loadRequest(request)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
 }
 
